c80f48714032aa68cc581a027d1087e2c381d673
njaswal4 <144952232+njaswal4@users.noreply.github.com>
2024-02-04 15:46:00 -0500

    Update q1.py

--- week4/q1.py
+++ week4/q1.py
@@ -1,1 +1,15 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
 def getPrimeNumbers(n):
+    return [num for num in range(2, n + 1) if is_prime(num)]
+
+# Example ->
+n = 15
+result = getPrimeNumbers(n)
+print(f"Prime numbers between 2 and {n}: {result}")
