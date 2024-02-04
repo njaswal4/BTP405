cdfd578ddcbcfe5a0c3acf76425e858fa3ed7e9c
njaswal4 <144952232+njaswal4@users.noreply.github.com>
2024-02-04 16:08:36 -0500

    Update q3.py

--- week4/q3.py
+++ week4/q3.py
@@ -1,1 +1,21 @@
-def wordCount(t):
+def wordCount(file_path):
+    word_dict = {}
+
+    with open(file_path, 'r') as file:
+        for line_number, line in enumerate(file, start=1):
+            words = line.strip().split()
+
+            for word in words:
+                clean_word = word.strip('.,?!:;').lower()
+
+                if clean_word not in word_dict:
+                    word_dict[clean_word] = [line_number]
+                else:
+                    word_dict[clean_word].append(line_number)
+
+    return word_dict
+
+# Example ->
+file_path = r'Text.txt'  
+result = wordCount(file_path)
+print(result)
